13af7732e6defdd8b7be237f160fdc002a13168a
holistudio <daniel.bin.lu@gmail.com>
2025-12-03 19:03:13 -0500

    Update mcts.py

--- src/agents/mcts/mcts.py
+++ src/agents/mcts/mcts.py
@@ -17,9 +17,13 @@ class Node(object):
         pass
 
     def sample_untried_actions(self):
-        # TODO: randomly choose untried action
-        # TODO: de-list tried action?
-        return
+        # print(f"before: {self.untried_actions}")
+        num_actions = len(self.untried_actions)
+        idx = random.randint(0,num_actions-1)
+        a = self.untried_actions.pop(idx)
+        # print(f"action: {a}")
+        # print(f"after: {self.untried_actions}")
+        return a
     
     def is_full_expanded(self):
         if len(self.untried_actions) == 0:
@@ -39,14 +43,16 @@ class UCTAgent(object):
         pass
 
     def expand(self, parent_node, parent_state):
-        action = parent_node.untried_actions
-        next_state, _ = self.env.transition(parent_state, action)
+        # print('### EXPANDING')
+        action = parent_node.sample_untried_actions()
+        next_state = self.env.transition(parent_state, action)
         next_actions = self.env.available_actions(next_state)
         next_node = Node(available_actions=next_actions, parent=parent_node, incoming_action=action)
         parent_node.children[action] = next_node
         return next_node, next_state
     
     def best_child(self, parent_node):
+        # print('### BEST CHILD')
         N = parent_node.N
         max_uct_node = None
         max_uct_value = - float('inf')
@@ -70,29 +76,34 @@ class UCTAgent(object):
         return max_uct_node
     
     def tree_policy(self, parent_node, parent_state):
+        # print('# TREE POLICY')
         while not self.env.check_terminal(parent_state):
-            if not parent_node.is_full_expanded:
+            if not parent_node.is_full_expanded():
+                # print('## EXPAND')
                 return self.expand(parent_node, parent_state)
             else:
+                # print('## EXPANDED => FIND BEST CHILD')
                 parent_node = self.best_child(parent_node)
                 a = parent_node.incoming_action
-                parent_state, _ = self.env.transition(parent_state, a)
+                parent_state = self.env.transition(parent_state, a)
         return parent_node, parent_state
     
     def default_policy(self, state):
+        # print('# DEFAULT POLICY')
         # TODO: restrict search depth somehow
         while not self.env.check_terminal(state):
             actions = self.env.available_actions(state)
             num_actions = len(actions)
             idx = random.randint(0,num_actions-1)
             a = actions[idx]
-            state, _ = self.env.transition(state, a)
-            outcome = self.env.outcome(state)
-            # TODO: re-consider where computations are accounted for
-            self.iter += 1
+            state = self.env.transition(state, a)
+        outcome = self.env.outcome(state)
+        if outcome > 0:
+            print(f'## terminal, outcome: {self.env.check_terminal(state)}, {outcome}')
         return outcome
     
     def backup_negamax(self, node, outcome):
+        # print('# BACKUP')
         while node is not None:
             node.N += 1
             node.Q.append(outcome)
@@ -102,13 +113,18 @@ class UCTAgent(object):
     def uct_search(self, initial_state):
         root_node = Node(self.env.available_actions(initial_state))
         while self.iter < self.max_iters:
+            if self.iter % 10000 == 0:
+                print(self.iter)
             new_node, new_state = self.tree_policy(root_node, initial_state)
             outcome = self.default_policy(new_state)
             self.backup_negamax(new_node, outcome)
+            # TODO: re-consider where computations are accounted for
+            self.iter += 1
         return self.best_child(root_node).incoming_action
 
-    def step(self):
-        pass
+    def step(self, obs):
+        self.iter = 0
+        return self.uct_search(obs)
 
     def act(self):
         pass
