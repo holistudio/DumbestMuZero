04321a97225c85cfe0221ce6b8a788d8ac80841f
holistudio <daniel.bin.lu@gmail.com>
2025-12-04 23:40:17 -0500

    Update mcts_test.py

--- src/mcts_test.py
+++ src/mcts_test.py
@@ -2,31 +2,30 @@ from envs.tictactoe import tictactoe
 from agents.mcts.mcts import UCTAgent
 
 env = tictactoe.env(render_mode="human")
-env.reset(seed=42)
+
 
 agent1 = UCTAgent(environment=env)
-agent_list = [agent1, agent1]
+agent2 = UCTAgent(environment=env, p1=False)
+agent_list = [agent1, agent2]
 
-idx = 0
 
-for a in env.agent_iter():
-    agent = agent_list[idx]
-    observation, reward, termination, truncation, info = env.last()
+for _ in range(5):
+    idx = 0
+    env.reset(seed=42)
+    for a in env.agent_iter():
+        agent = agent_list[idx]
+        observation, reward, termination, truncation, info = env.last()
 
-    if termination or truncation:
-        action = None
-    else:
-        mask = observation["action_mask"]
-        if idx == 0:
-            # action = env.action_space(a).sample(mask)
-            action = agent.step(observation)
+        if termination or truncation:
+            action = None
         else:
-            action = env.action_space(a).sample(mask)
-            # action = agent.step(observation)
+            mask = observation["action_mask"]
+            if agent == 'random':
+                action = env.action_space(a).sample(mask)
+            else:
+                action = agent.step(observation)
 
-    env.step(action)
-    idx += 1
-    if idx >= len(agent_list):
-        idx = 0
-pause = input('press enter')
+        env.step(action)
+        idx = (idx+1) % 2
+    pause = input('press enter for new game')
 env.close()
