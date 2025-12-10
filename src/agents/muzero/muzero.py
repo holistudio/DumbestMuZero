ae842d0703070764bafa82eae3976520372ce74a
holistudio <daniel.bin.lu@gmail.com>
2025-12-10 18:07:04 -0500

    Update muzero.py
    Attempt at the forward pass / planning

--- src/agents/muzero/muzero.py
+++ src/agents/muzero/muzero.py
@@ -3,23 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Node(object):
-    def __init__(self, state, policy, value, available_actions, parent=None, incoming_action=None):
+    def __init__(self, state, u, policy, value, available_actions, parent=None, incoming_action=None):
         self.parent = parent
         self.children = {} # keys are actions, values are Nodes
 
         self.state = state
+        self.reward = u
         self.policy = policy
         self.value = value
 
+        self.N = 0
+
+        self.legal_actions = available_actions
         self.untried_actions = available_actions
         self.incoming_action = incoming_action
         pass
-
+    
     def sample_untried_actions(self):
+        def tensor_delete(tensor, indices):
+            mask = torch.ones(tensor.numel(), dtype=torch.bool)
+            mask[indices] = False
+            return tensor[mask]
         # print(f"before: {self.untried_actions}")
         num_actions = len(self.untried_actions)
         idx = torch.randint(0, num_actions)
-        a = self.untried_actions.pop(idx)
+        a = self.untried_actions[idx].item()
+        self.untried_actions = tensor_delete(self.untried_actions, idx)
         # print(f"action: {a}")
         # print(f"after: {self.untried_actions}")
         return a
@@ -85,24 +94,61 @@ class PredictionFunction(nn.Module):
     
 
 class MCTS(nn.Module):
-    def __init__(self, input_size, state_size, policy_size, hidden_size, K):
+    def __init__(self, input_size, state_size, policy_size, hidden_size, action_size, K):
         super().__init__()
         self.rep = StateFunction(input_size, state_size, hidden_size)
         self.dynamics = DynamicsFunction(input_size, state_size, hidden_size)
         self.prediction = PredictionFunction(input_size, policy_size, hidden_size)
 
         self.K = K
+        self.max_iters = 10
+        self.action_size = action_size
+        self.action_space = torch.arange(0,action_size)
         pass
 
-    def forward(self, obs):
-        state = self.rep(obs)
-        tree = {} # TODO: figure out tree structure for exploring actions and tracking function outputs
+    def expand(self, parent_node):
+        action = parent_node.sample_untried_actions()
+        return action
+    
+    def best_child(self, parent_node):
+        # TODO: select next best child using values in each child node?
+        max_node = None
+        max_value = -float('inf')
+        for a in sorted(parent_node.children):
+            child_node = parent_node.children[a]
+            value = child_node.value
+            if value > max_value:
+                max_value = value
+                max_node = child_node
+        return max_node
+    
+    def tree_policy(self, parent_node):
+        terminal = False
+        while not terminal:
+            if not parent_node.is_full_expanded():
+                return self.expand(parent_node)
+            else:
+                next_node = self.best_child(parent_node)
+                if next_node is None:
+                    terminal = True
+        return parent_node
+    
+    def default_policy(self, current_node):
         for _ in range(self.K):
-            # TODO: this loop needs to use something akin to tree policy and default policy, probably
-            action = todo() # TODO: sample available and unxplored actions only 
+            action = current_node.sample_untried_actions()
             state, reward = self.dynamics(state, action)
             policy_params, value = self.prediction(state)
-        return tree # TODO: this will need to be modified to get loss.backward() to work
+            new_node = Node(state, reward, policy_params, value, self.action_space, current_node, action)
+            current_node = new_node
+        pass
+    
+    def forward(self, obs):
+        state = self.rep(obs)
+        policy_params, value = self.prediction(state)
+        current_node = Node(state, 0, policy_params, value, self.action_size)
+        new_node = self.tree_policy(current_node)
+        self.default_policy(new_node)
+        return current_node # TODO: this will need to be modified to get loss.backward() to work
 
 class MuZeroAgent(object):
     def __init__(self, input_size, state_size, policy_size, hidden_size, K=10, c=1.0):
@@ -115,19 +161,19 @@ class MuZeroAgent(object):
         # TODO: given action probabilities, sample action
         return action
     
-    def policy_function(self, tree):
+    def policy_function(self, root_node):
         # TODO: use policies from MCTS to produce a policy
         # and output action scores
         return action_scores
     
-    def sample_policy(self, tree, action_mask):
-        action_scores = self.policy_function(tree)
+    def sample_policy(self, root_node, action_mask):
+        action_scores = self.policy_function(root_node)
         action_scores = action_scores[action_mask]
         action_probs = torch.softmax(action_scores)
         action = self.sample_action(action_probs)
         return action
     
-    def value_function(self, tree):
+    def value_function(self, root_node):
         # TODO: use values predicted during MCTS to output a final value estimate
         return v
     
@@ -138,9 +184,9 @@ class MuZeroAgent(object):
 
     def step(self, obs, action_mask):
         obs = self.preprocess_obs(obs)
-        tree = MCTS(obs)
-        action =  self.sample_policy(tree, action_mask)
-        value = self.value_function(tree)
+        root_node = MCTS(obs)
+        action =  self.sample_policy(root_node, action_mask)
+        value = self.value_function(root_node)
         # TODO: push action and value to ReplayBuffer
         return action, value
     
