f59459b8cad907e98797c26ed7184d27893d0f0d
holistudio <daniel.bin.lu@gmail.com>
2025-12-12 11:35:20 -0500

    Update mu_mcts.py
    - add edge references to parent node and incoming action
    - since edge contains incoming action, have nodes reference an incoming edge instead

--- src/agents/muzero/mu_mcts.py
+++ src/agents/muzero/mu_mcts.py
@@ -1,16 +1,19 @@
 class TreeEdge(object):
     def __init__(self, parent_node, action, P):
+        self.parent_node = parent_node
+        self.action = action
+
         self.N, self.Q = 0, 0
         self.P = P
         self.S, self.R = None, 0
         pass
 
 class TreeNode(object):
-    def __init__(self, state, policy, action_space, parent=None, incoming_action=None):
+    def __init__(self, state, policy, action_space, parent=None, edge=None):
         self.state = state
         self.edges = {} # key: action, value: TreeEdge()
         for a in action_space:
             self.edge[a] = TreeEdge(self, a, policy)
             self.parent = parent
-        self.incoming_action = incoming_action
+        self.edge = edge
         pass
