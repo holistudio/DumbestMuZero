66011a39b040547f29e1a773a0398940a3b98377
holistudio <daniel.bin.lu@gmail.com>
2025-12-21 21:53:02 -0500

    Update muzero_train.py
    - modify config for quick debugging
    - agent updates experience every step OR at the very moment the game terminates and NOT afterwards (revisit this later)

--- src/muzero_train.py
+++ src/muzero_train.py
@@ -1,17 +1,19 @@
 from envs.tictactoe import tictactoe
 from agents.muzero.muzero import MuZeroAgent
 
-env = tictactoe.env(render_mode="human")
+# env = tictactoe.env(render_mode="human")
+env = tictactoe.env()
 
 config = {
     'batch_size': 8,
-    'buffer_size': 1000,
+    'buffer_size': 10,
     'state_size': 16,
     'hidden_size': 64,
     'lr': 3e-4,
     'weight_decay': 1e-4,
-    'max_iters': 10_000,
-    'gamma': 0.997
+    'max_iters': 10,
+    'gamma': 0.997,
+    'k_unroll_steps': 5
 }
 
 agent1 = MuZeroAgent(environment=env, config=config)
@@ -19,8 +21,11 @@ agent1 = MuZeroAgent(environment=env, config=config)
 agent_list = [agent1 , 'random']
 
 
-for _ in range(5):
+for ep in range(5):
+    print(f'EP={ep}')
     idx = 0
+    p1_reward, p2_reward = 0, 0
+    recorded = False
     env.reset(seed=42)
     for a in env.agent_iter():
         agent = agent_list[idx]
@@ -28,18 +33,35 @@ for _ in range(5):
 
         if termination or truncation:
             action = None
+            if not recorded:
+                p1_reward, p2_reward = env.rewards['player_1'], env.rewards['player_2']
+                recorded = True
+                if a == 'player_1':
+                    agent1.experience(observation, a, action, 
+                                    p1_reward, p1_reward, termination)
+                else:
+                    agent1.experience(observation, a, action, 
+                                    p2_reward, p2_reward, termination)
         else:
-            if a == 'player_1':
-                print('\nPLAYER X TURN')
-            else:
-                print('\nPLAYER O TURN')
+            # if a == 'player_1':
+            #     print('\nPLAYER X TURN')
+            # else:
+            #     print('\nPLAYER O TURN')
             mask = observation["action_mask"]
             if agent == 'random':
-                action = env.action_space(a).sample(mask)
+                action = int(env.action_space(a).sample(mask))
             else:
                 action = agent.step(observation)
 
         env.step(action)
+        if not recorded:
+            if a == 'player_1':
+                agent1.experience(observation, a, action, 
+                                p1_reward, p1_reward, termination)
+            else:
+                agent1.experience(observation, a, action, 
+                                p2_reward, p2_reward, termination)
         idx = (idx+1) % 2
-    pause = input('\npress enter for new game')
+    agent1.update()
+    # pause = input('\npress enter for new game')
 env.close()
