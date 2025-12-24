0e3f21746480a5a555962903922a09b964f135a5
holistudio <daniel.bin.lu@gmail.com>
2025-12-24 12:15:42 -0500

    Update train.py
    - evaluate agent against random agent to monitor performance over training

--- src/train.py
+++ src/train.py
@@ -50,6 +50,84 @@ for ep in range(100):
         agent.experience(observation, a, action, env.rewards[a], termination)
 
     agent1.update()
-    print(f'{datetime.datetime.now()-start_time} EP={ep}')
+    if (ep+1) % 10 == 0:
+        print(f'{datetime.datetime.now()-start_time} EP={ep}')
+        eval(agent1,ep)
     # pause = input('\npress enter for new game')
 env.close()
+
+def eval(rl_agent, ep):
+    p1_w_l_d = [0, 0, 0]
+    p2_w_l_d = [0, 0, 0]
+
+    env = tictactoe.env()
+    agents = {
+        'player_1': rl_agent,
+        'player_2': 'random'
+    }
+
+    
+
+    for ep in range(100):
+        env.reset(seed=42)
+        for a in env.agent_iter():
+            agent = agents[a]
+            observation, reward, termination, truncation, info = env.last()
+            
+            if termination or truncation:
+                action = None
+
+            else:
+                mask = observation["action_mask"]
+                if agent == 'random':
+                    action = int(env.action_space(a).sample(mask))
+                else:
+                    action = agent.act(observation)
+
+            env.step(action)
+            if env.terminations[a] == True:
+                if env.rewards['player_1'] == 1:
+                    p1_w_l_d[0] += 1
+                elif env.rewards['player_1'] == -1:
+                    p1_w_l_d[1] += 1
+                else:
+                    p1_w_l_d[2] += 1
+    env.close()
+
+    env = tictactoe.env()
+    agents = {
+        'player_1': 'random',
+        'player_2': rl_agent
+    }
+
+    for ep in range(100):
+        env.reset(seed=42)
+        for a in env.agent_iter():
+            agent = agents[a]
+            observation, reward, termination, truncation, info = env.last()
+            
+            if termination or truncation:
+                action = None
+
+            else:
+                mask = observation["action_mask"]
+                if agent == 'random':
+                    action = int(env.action_space(a).sample(mask))
+                else:
+                    action = agent.act(observation)
+
+            env.step(action)
+            if env.terminations[a] == True:
+                if env.rewards['player_2'] == 1:
+                    p2_w_l_d[0] += 1
+                elif env.rewards['player_2'] == -1:
+                    p2_w_l_d[1] += 1
+                else:
+                    p2_w_l_d[2] += 1
+    env.close()
+
+    p1_w_perc = p1_w_l_d[0] * 100 / sum(p1_w_l_d)
+    p2_w_perc = p2_w_l_d[0] * 100 / sum(p2_w_l_d)
+
+    print(f'EP={ep} Agent Performance, as P1: {p1_w_perc:.2f}%, {p1_w_l_d}, as P2: {p2_w_perc:.2f}%, {p2_w_l_d}')
+    pass
