9cc73ff0559a5bb9e1e2fa49c07e4992d5f7ca03
holistudio <daniel.bin.lu@gmail.com>
2026-07-17 00:47:12 -0400

    constant temperature within game
    sample actions with same temperature throughout the game regardless of number of moves

--- src/agents/muzero/muzero.py
+++ src/agents/muzero/muzero.py
@@ -848,16 +848,17 @@ class MuZeroAgent(object):
         """
         obs = self.preprocess_obs(observation)
         
-        blanks = torch.where(obs == 0)[0].tolist()
-        
         # simple temperature annealing for tic-tac-toe:
         # if both players have placed two pieces (5 or fewer blank spaces), 
         # there is likely a single move to exploit
         # that is best to block of connect 3 in a row
-        if len(blanks) > 5:
-            action = self.search(obs, self.current_temperature())
-        else:
-            action = self.search(obs, 0.0)
+        # blanks = torch.where(obs == 0)[0].tolist()
+        # if len(blanks) > 5:
+        #     action = self.search(obs, self.current_temperature())
+        # else:
+        #     action = self.search(obs, 0.0)
+        
+        action = self.search(obs, self.current_temperature())
 
         return action
     
