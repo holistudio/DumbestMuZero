ce1d0b1be9def4dbefc96ce6692f25a1d52366fb
holistudio <daniel.bin.lu@gmail.com>
2026-08-15 16:02:10 -0400

    add comments to MCTS

--- src/agents/mcts/mcts.py
+++ src/agents/mcts/mcts.py
@@ -20,9 +20,11 @@ class Node(object):
 
     def sample_untried_actions(self):
         # print(f"before: {self.untried_actions}")
+
         num_actions = len(self.untried_actions)
         idx = np.random.randint(0,num_actions)
         a = self.untried_actions.pop(idx)
+
         # print(f"action: {a}")
         # print(f"after: {self.untried_actions}")
         return a
@@ -44,17 +46,23 @@ class UCTAgent(object):
 
     def expand(self, parent_node, parent_state):
         # print('### EXPANDING')
+
         action = parent_node.sample_untried_actions()
         next_state = self.env.transition(parent_state, action)
+
         # print('### NEXT STATE')
         # display_board(next_state)
+
         next_actions = self.env.available_actions(next_state)
         next_node = Node(state = next_state, available_actions=next_actions, parent=parent_node, incoming_action=action)
         parent_node.children[action] = next_node
+
         # pause = input('###')
+
         return next_node, next_state
     
     def best_child(self, parent_node, parent_state):
+        # use UCT/UCB1 to select child node
         N = parent_node.N
         max_uct_node = None
         max_uct_value = -float('inf')
@@ -72,6 +80,7 @@ class UCTAgent(object):
                 exploitation_term = (q/n_child)
                 exploration_term = 2*self.C_p*np.sqrt(2*np.log(N)/n_child)
                 uct_value = exploitation_term + exploration_term
+
             if uct_value > max_uct_value:
                 max_uct_value = uct_value
                 max_uct_node = child_node
@@ -80,24 +89,36 @@ class UCTAgent(object):
     def tree_policy(self, parent_node, parent_state):
         # print('# TREE POLICY')
         # print(f'# Terminal?: {self.env.check_terminal(parent_state)}')
+
+        # selection, finding the most urgent expandable node
         while not self.env.check_terminal(parent_state):
             if not parent_node.is_full_expanded():
+                # expansion
+
                 # print('## EXPAND')
                 return self.expand(parent_node, parent_state)
             else:
+                # select child node based on UCB1/UCT
+
                 # print('## EXPANDED => FIND BEST CHILD')
                 parent_node = self.best_child(parent_node, parent_state)
                 action = parent_node.incoming_action
                 parent_state = self.env.transition(parent_state, action)
+
                 # print('## BEST CHILD STATE')
                 # display_board(parent_state)
+
         # pause = input('# END OF TREE POLICY ITER')
+
         return parent_node, parent_state
     
     def default_policy(self, state):
         # print('\n\n# LEAF START BOARD')
         # display_board(state)
         # print('# DEFAULT POLICY')
+
+        # simulation, random play turn-by-turn
+        # until simulated game is done
         steps = 0
         while not self.env.check_terminal(state):
             actions = self.env.available_actions(state)
@@ -105,29 +126,39 @@ class UCTAgent(object):
             idx = np.random.randint(0,num_actions)
             action = actions[idx]
             state = self.env.transition(state, action)
+
             # print(f'## NEXT SIM STATE {steps}')
             # print(state["observation"][:,:,0])
             # print(state["observation"][:,:,1])
             # display_board(state)
+
             steps += 1
+
             # pause = input('##')
+
         # backup outcome sign depends which player moved INTO the leaf node
         # relative to that player, odd number of steps suggests that player won
         # even number of steps suggests player lost
         outcome = self.env.outcome(state) if (steps % 2 == 1) else -self.env.outcome(state)
+
         # print(f'# terminal, outcome: {self.env.check_terminal(state)}, {outcome}, steps={steps}')
         # pause = input('# END OF SIM\n')
+
         return outcome
     
     def backup_negamax(self, node, outcome):
         # print(f'# BACKUP RECEIVES Outcome: {outcome}\n')
+
+        # backpropagate outcome to root node
         while node is not None:
             # display_board(node.state)
             # print(f"# BOARD Outcome: {outcome}\n")
+
             node.N += 1
             node.Q.append(outcome)
-            outcome = -outcome
+            outcome = -outcome # two-player, zero-sum, adversarial games
             node = node.parent
+
         # pause = input('# END BACKUP\n')
     
     def final_action(self, root_node, initial_state):
@@ -138,24 +169,39 @@ class UCTAgent(object):
         
         for a in sorted(root_node.children):
             child_node = root_node.children[a]
+
+            # UCT value computation
             avg_q = sum(child_node.Q) / child_node.N if child_node.N > 0 else 0
             exploration_term = 2*self.C_p*np.sqrt(2*np.log(N)/child_node.N) if child_node.N > 0 else float('inf')
+
             print(f"### Action={a}, Num Visits= {child_node.N}({child_node.N/root_node.N:.2f}), Expected Value= {avg_q:.2f}, UCT Value= {avg_q+exploration_term:.2f}")
+
+            # select best action based on node with greatest number of visits
             if child_node.N > max_visits:
                 max_visits = child_node.N
                 best_action = a
+
         print(f"\n# PLACE at CELL {best_action}")
         display_board(self.env.transition(initial_state, best_action))
+        
         # pause = input('#')
+
         return best_action
 
     def uct_search(self, initial_state):
         root_node = Node(initial_state, self.env.available_actions(initial_state))
         for _ in range(self.max_iters):
             # print(self.iter)
+
+            # selection and expansion
             new_node, new_state = self.tree_policy(root_node, initial_state)
+
+            # simulation
             outcome = self.default_policy(new_state)
+
+            # backpropagation
             self.backup_negamax(new_node, outcome)
+
             # pause = input('END OF SEARCH ITER')
         # print('END OF UCT SEARCH')
 
